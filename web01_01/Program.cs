77d16a1b56f723fb36493a1cfe3197109cdf8d13
Vika <nika-maks@inbox.ru>
2022-03-01 18:46:37 +0200

    Принимает число и выдает его квадрат

---
+++ web01_01/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Введите число"); // Вывод приглашения для ввода
+string strValue = Console.ReadLine(); // Ввод строки
+int value = int.Parse(strValue); // Преобразование строки в целое число
+
+int result = value * value; //Вычисление квадрата
+Console.WriteLine(result); // вывод результата
+
+
