f73f29a245d65d2c582f41e464251c5582fa6cc7
Vika <nika-maks@inbox.ru>
2022-03-02 21:21:41 +0200

    дз задача 1.2

---
+++ Homework1.2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите число А ->  ");
+string inputA = Console.ReadLine();
+int A = int.Parse(inputA);
+
+Console.WriteLine("Введите число B ->  ");
+string inputB = Console.ReadLine();
+int B = int.Parse(inputB);
+
+Console.WriteLine("Введите число C ->  ");
+string inputC = Console.ReadLine();
+int C = int.Parse(inputC);
+
+int max = A;
+
+if (B> max);
+    max= B;
+if (C> max)   
+    max= C;
+if (A> max) 
+    max= A;
+Console.WriteLine($"Максимальное число- {max}");
+
