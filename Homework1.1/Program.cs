41c23162ed0b359a1e7f37b8a0810a5d9d08258e
Vika <nika-maks@inbox.ru>
2022-03-02 19:09:06 +0200

    дз задача 1.1

---
+++ Homework1.1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа 
+// и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите число А ->  ");
+string inputA = Console.ReadLine();
+int A = int.Parse(inputA);
+
+Console.WriteLine("Введите число B ->  ");
+string inputB = Console.ReadLine();
+int B = int.Parse(inputB);
+
+if (A > B) Console.WriteLine(inputA + ">" + inputB);
+else
+{
+Console.WriteLine(inputB + ">"+ inputA);
+}
+
+
+
+
+
+
