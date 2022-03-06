72ebfc7c6aa34eeac12251b89a273ddfc79a932d
Vika <nika-maks@inbox.ru>
2022-03-06 17:50:50 +0200

    Найти сумму 2 целых чисел

---
+++ ex003Sum/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача: найти сумму 2 целых чисел
+//int numberA = 3;
+//int numberB = 5;
+//Console.WriteLine(numberA + numberB);
+
+// или
+
+int numberA = 7;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
