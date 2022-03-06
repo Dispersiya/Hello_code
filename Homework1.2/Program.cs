cb99b37c3e1c1d3290ffe2e5ef92a2e46c2ca4b0
Vika <nika-maks@inbox.ru>
2022-03-06 21:46:00 +0200

    изменения в дз 1.2

--- Homework1.2/Program.cs
+++ Homework1.2/Program.cs
@@ -13,12 +13,23 @@ string inputC = Console.ReadLine();
 int C = int.Parse(inputC);
 
 int max = A;
-
-if (B> max);
-    max= B;
-if (C> max)   
-    max= C;
-if (A> max) 
-    max= A;
+if (A> max) max = A;
+if (B> max) max = B;
+if (C> max) max = C;
 Console.WriteLine($"Максимальное число- {max}");
+
+
+
+
+
+
+
+
+//if (B> max);
+//   max= B;
+//if (C> max)   
+ //   max= C;
+//if (A> max) 
+ //   max= A;
+//Console.WriteLine($"Максимальное число- {max}");
 
