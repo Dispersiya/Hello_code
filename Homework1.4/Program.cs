787cc7be0666901bf60baade80e6c036ecbbe530
Vika <nika-maks@inbox.ru>
2022-03-07 09:22:57 +0200

    дз 1.4

---
+++ Homework1.4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write ("Введите число ");
+string  numberN= Console.ReadLine();
+int N = int.Parse(numberN);
+
+int count=1;
+while (count <= N)
+{
+if (count%2==0)
+    Console.WriteLine($"{count} - четное");
+count =count +1;     
+}
+
+
+
