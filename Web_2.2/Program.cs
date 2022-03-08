b0830054c54a5e60229c6ea9111ff2b813018917
Vika <nika-maks@inbox.ru>
2022-03-08 13:50:50 +0200

    random

---
+++ Web_2.2/Program.cs
@@ -0,0 +1,17 @@
+﻿// 2. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа. Например,78 -> 8 , 12-> 2, 85 -> 8
+
+Random rnd = new Random();
+
+int value = rnd.Next(10,99);
+while (value != 98)
+{
+    Console.WriteLine($"Случайное значение= {value}");
+    value = rnd.Next(10,99);
+}
+int Randomint (int min, int max)
+{
+    Random rnd = new Random();
+    int value =rnd.Next(min,max);
+    return value;
+}
