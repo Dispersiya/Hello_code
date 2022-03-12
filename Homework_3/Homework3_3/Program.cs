90531c91d19f6249a29541b34b59f267af1b244a
Vika <nika-maks@inbox.ru>
2022-03-12 13:15:29 +0200

    а вход число (N) и выдаёт таблицу кубов чисел от 1 до N

---
+++ Homework_3/Homework3_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 4, 9.
+// 5 -> 1, 8, 27, 64, 125
+
+int Promt (string message)
+{
+    Console.WriteLine(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+int N = Promt("Введите число =>");
+
+int A = 1;
+Console.Write( A + " ");
+while (A < N)
+{
+    A= A+1;
+    Console.WriteLine(A*A*A + " ");
+}
+
