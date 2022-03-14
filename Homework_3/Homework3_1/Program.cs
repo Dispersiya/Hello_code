1fd562e9c75caa810b4c8c75bceaa4eb206b2dfa
Vika <nika-maks@inbox.ru>
2022-03-14 18:52:48 +0200

    является ли чило палиндромом вар.1

--- Homework_3/Homework3_1/Program.cs
+++ Homework_3/Homework3_1/Program.cs
@@ -1,2 +1,9 @@
 ﻿// Задача 1: Напишите программу, которая принимает на вход 
 // пятизначное число и проверяет, является ли оно палиндромом.
+
+
+Console.WriteLine("Введите число");
+string n = Console.ReadLine();
+int len= n.Length;
+if ((n[0]== n[4]) && (n[1]== n[3])) Console.WriteLine("palindrom");
+else Console.WriteLine("no palindrom");
