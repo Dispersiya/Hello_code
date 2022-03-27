d1f112d688fd027ada926b4cacb7acde1b7a5bb1
Vika <nika-maks@inbox.ru>
2022-03-27 14:36:49 +0300

    задаёт массив из 8 элементов

---
+++ Homework4.3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+//случайными числами и выводит их на экран.
+int[] Array = new int[8];
+Random rnd = new Random(); 
+for (int i=0; i<8; i++)
+{
+Array[i] = rnd.Next(1,999);
+Console.Write($"{Array[i]} ");
+}
+Console.WriteLine();
