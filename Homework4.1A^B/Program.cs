cbb1e3973f7499fb699d8290e01b321b1a32ebee
Vika <nika-maks@inbox.ru>
2022-03-27 15:40:00 +0300

    принимает на вход два числа (A и B) и возводит число A в натуральную степень B

---
+++ Homework4.1A^B/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+Console.WriteLine("Введите число a"); 
+int a= int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b= int.Parse(Console.ReadLine());
+
+int result = 1;
+for (int i = 0; i < b; i ++)
+{
+    result*=a;
+}
+Console.WriteLine($"Число {a} в степени {b} = {result}");
