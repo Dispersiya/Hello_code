98923880b0b6e13297de836d2569701e4d1c6ae6
Vika <nika-maks@inbox.ru>
2022-04-10 16:23:00 +0300

    Массив заполненный случ.вещ.числами. Такого элемента нет. Среднее арифметическое элементов в столбце

---
+++ Homework7.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+double[,] a = new double[5, 10];
+Random random = new Random();
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        a[i, j] = random.NextDouble() * 100; 
+        Console.Write("{0,6:F2}", a[i, j]);
+    }
+    Console.WriteLine();
+}
