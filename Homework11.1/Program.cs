5d80ff50aa97b5021c9c80b3c690882371068092
Vika <nika-maks@inbox.ru>
2022-04-17 13:45:44 +0300

    11.1

---
+++ Homework11.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Определите, является ли число степенью двойки:
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        if (step(a)) Console.WriteLine("Является степенью двойки");
+        else Console.WriteLine("Не являетя степенью двойки");
+        Console.ReadKey();
+    }
+    public static bool step(int a)
+    {
+        if (a == 2) return true;
+        else if (a % 2 == 0) return step(a / 2);
+        else return false;
+    }
+}
