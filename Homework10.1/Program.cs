9e9b70d79864192f1853d2a35237fbdc6cd22452
Vika <nika-maks@inbox.ru>
2022-04-17 16:00:04 +0300

    Массив 10х представлений чисел массива data.Разбиевние на группы  N ≤ 10²⁰

--- Homework10.1/Program.cs
+++ Homework10.1/Program.cs
@@ -6,3 +6,38 @@
 //info = {2, 3, 3, 1 }
 //выходные данные:
 //1, 7, 0, 1
+
+
+
+void FillArray(int[] array, int minValue, int maxValue)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = new Random().Next(minValue, maxValue);
+	}
+
+}
+
+int[] data = {0, 1, 1, 1, 1, 0, 0, 0, 1 };
+
+int[] info = {2, 3, 3, 1 };
+
+List<int> namberList = new List<int>();
+int index = 0;
+for (int i = 0; i < info.Length; i++)
+{
+	int namber = 0;
+	for (int j = 0; j < info[i]; j++)
+	{
+		namber += data[index] * Convert.ToInt32(Math.Pow(2, info[i] - j -1));
+		index++;
+	}
+	namberList.Add(namber);
+}
+
+foreach (int namber in namberList)
+{
+	Console.Write($"{namber}, ");
+}
+
+
